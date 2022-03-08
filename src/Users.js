b22a94638deddafed84f17a8adf4eeb551956e75
Damhee Kim <ekal.kdh@gmail.com>
2022-03-08 22:39:41 +0900

    api with reducer

--- src/Users.js
+++ src/Users.js
@@ -1,29 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import axios from 'axios'
 
+function reducer(state, action) {
+    switch(action.type) {
+        case "LOADING" :
+            return {
+                loading: true,
+                data: null,
+                error: null
+            }
+        case "SUCCESS" :
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            }
+        case "ERROR" :
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            }
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`) 
+    }
+}
+
 function Users() {
-    const [users, setUsers] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: null,
+        error: null
+    })
 
     const fetchUsers = async() => {
+        dispatch({ type: 'LOADING' })
         try {
-            setUsers(null)
-            setError(null)
-            setLoading(true)
-
             const response = await axios.get('http://jsonplaceholder.typicode.com/users')
-            setUsers(response.data)
+            dispatch({type: 'SUCCESS', data: response.data})
         } catch (e) {
-            setError(e)
+            dispatch({type: 'ERROR', error: e})
         }
-        setLoading(false) // loading is done
     }
 
     useEffect(() => {
-        
         fetchUsers()
     }, [])
+
+    const {loading, users, error} = state
 
     if(loading) {
         return <div>로딩중..</div>
